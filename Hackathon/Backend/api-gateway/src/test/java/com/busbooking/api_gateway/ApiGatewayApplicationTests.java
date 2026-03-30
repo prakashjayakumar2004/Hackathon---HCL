f2c31d75b7162663a2f31d7602c71425d6699d4b
sandeep <spsandeep170828@gmail.com>
2026-03-30 15:27:11 +0530

    commited

---
+++ Hackathon/Backend/api-gateway/src/test/java/com/busbooking/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busbooking.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
